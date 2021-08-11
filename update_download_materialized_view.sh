bec3fc5005aecf0eda9c8cbc921d80f05eee8322
Rodrigo Mariano <rodmariano13@gmail.com>
2021-08-11 09:54:41 -0300

    chore: update bash script to restart catalog_dash

--- update_download_materialized_view.sh
+++ update_download_materialized_view.sh
@@ -6,5 +6,8 @@ docker exec inpe_cdsr_postgis \
     psql -h localhost -U postgres -d cdsr_register \
         -c 'CALL update_download_view_number_of_assets();'
 
+# restart dashboard after updating the database
+docker restart inpe_cdsr_catalog_dash
+
 echo "file has been executed at $(date)"
 
