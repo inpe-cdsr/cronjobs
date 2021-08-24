1f686d8cf643aeb69cb96abab55dc951c4032b62
Rodrigo Mariano <rodmariano13@gmail.com>
2021-08-24 09:58:58 -0300

    feat: update script to add new container to be restarted

--- restart_containers.sh
+++ restart_containers.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# restart dashboardm in order to update start and end dates from date picker range
-# (no, date picker range do not update automatically, unfortunately)
+# restart dashboards in order to update start and end dates from date picker range
+# (no, date picker range does not update automatically, unfortunately)
 docker restart inpe_cdsr_catalog_dash
+docker restart inpe_cdsr_publisher_dash
 
